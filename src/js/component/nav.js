c207231f71db85b4fb21e331db68630e83e71357
Homero Luzardo <homeroeluzardo@gmail.com>
2019-07-27 13:20:01 +0000

    Update2

--- src/js/component/nav.js
+++ src/js/component/nav.js
@@ -7,41 +7,43 @@ import rigoImage from "../../img/rigo-baby.jpg";
 export class Nav extends React.Component {
 	render() {
 		return (
-			<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-				<div class="container">
-					<a class="navbar-brand" href="#">
+			<nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+				<div className="container">
+					<a className="navbar-brand" href="#">
 						Start Bootstrap
 					</a>
 					<button
-						class="navbar-toggler"
+						className="navbar-toggler"
 						type="button"
 						data-toggle="collapse"
 						data-target="#navbarResponsive"
 						aria-controls="navbarResponsive"
 						aria-expanded="false"
 						aria-label="Toggle navigation">
-						<span class="navbar-toggler-icon" />
+						<span className="navbar-toggler-icon" />
 					</button>
-					<div class="collapse navbar-collapse" id="navbarResponsive">
-						<ul class="navbar-nav ml-auto">
-							<li class="nav-item active">
-								<a class="nav-link" href="#">
+					<div
+						className="collapse navbar-collapse"
+						id="navbarResponsive">
+						<ul className="navbar-nav ml-auto">
+							<li className="nav-item active">
+								<a className="nav-link" href="#">
 									Home
-									<span class="sr-only">(current)</span>
+									<span className="sr-only">(current)</span>
 								</a>
 							</li>
-							<li class="nav-item">
-								<a class="nav-link" href="#">
+							<li className="nav-item">
+								<a className="nav-link" href="#">
 									About
 								</a>
 							</li>
-							<li class="nav-item">
-								<a class="nav-link" href="#">
+							<li className="nav-item">
+								<a className="nav-link" href="#">
 									Services
 								</a>
 							</li>
-							<li class="nav-item">
-								<a class="nav-link" href="#">
+							<li className="nav-item">
+								<a className="nav-link" href="#">
 									Contact
 								</a>
 							</li>
